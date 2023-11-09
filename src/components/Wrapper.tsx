bdaa11bafdb1986b07e36e2ff629c07ba1437227
DJ Corbett <dcorbett@yext.com>
2023-11-09 15:57:12 -0500

    bot scoped nav option

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { cn } from '../utils/cn';
 import Logo from './Logo';
 import { Toaster } from './Toaster';
+import { ArrowLeft, MessageCircle, Search } from 'lucide-react';
 
 export interface WrapperProps {
   children: React.ReactNode;
@@ -21,22 +22,33 @@ const Wrapper = ({ children }: WrapperProps) => {
             </div>
             <h1 className="text-white text-lg font-semibold">Chat Visual Editor</h1>
           </div>
-          <nav className="flex flex-1 flex-col">
-            <ul role="list" className="flex flex-1 flex-col">
-              <li className="flex flex-col gap-2">
-                <a
-                  href="#"
-                  className={cn(
-                    selectedNavItem === 'Bots'
-                      ? 'bg-gray-800 text-white'
-                      : 'text-gray-400 hover:text-white hover:bg-gray-800',
-                    'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                  )}
-                  onClick={() => setSelectedNavItem('Bots')}
-                >
-                  All Chatbots
-                </a>
-              </li>
+          <nav className="flex flex-1 flex-col gap-8">
+            <div className="text-gray-400 text-sm flex items-center gap-5 border border-gray-400 p-2 rounded-full">
+              <Search className="w-4" /> Quick Find
+            </div>
+            <div className="text-gray-400 flex items-center gap-5">
+              <ArrowLeft className="w-5" /> Back
+            </div>
+            <div className="text-gray-400 flex flex-col gap-6">
+              <div className="text-white font-bold flex items-center gap-5">
+                <MessageCircle /> Salesforce Lead Gen
+              </div>
+              <div className="text-sm flex flex-col gap-3 pl-12">
+                <span className="text-white">Configuration</span>
+                <span className="pl-2">Goals</span>
+                <span className="pl-2">General Settings</span>
+                <span className="pl-2">Edit as JSON</span>
+              </div>
+              <div className="text-sm flex flex-col gap-3 pl-12">
+                <span className="text-white">Anaytics and Logging</span>
+                <span className="pl-2">Response Logs</span>
+              </div>
+              <div className="text-sm flex flex-col gap-3 pl-12">
+                <span className="text-white">Deployment</span>
+                <span className="pl-2 bg-gray-800 text-white font-semibold rounded-md p-2">Widgets</span>
+              </div>
+            </div>
+            {/* <ul role="list" className="flex flex-1 flex-col gap-y-7">
               <li className="flex flex-col gap-2">
                 <a
                   href="#"
@@ -81,7 +93,7 @@ const Wrapper = ({ children }: WrapperProps) => {
                   </li>
                 </ul>
               </li>
-            </ul>
+            </ul> */}
           </nav>
         </div>
       </div>
